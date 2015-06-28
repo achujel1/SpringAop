a4862bc7d781c1f82f76f8197369fdd1ad0ee5fc
achujel1 <achujel1@gmail.com>
2015-06-28 20:54:05 +0300

    Spring Aop
    Spring Aop

---
+++ src/org/roba/springaop/AopMain.java
@@ -0,0 +1,15 @@
+package org.roba.springaop;
+
+import org.roba.springaop.service.ShapeService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AopMain {
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(
+				"spring.xml");
+		ShapeService shapeService = ctx.getBean("shapeService",
+				ShapeService.class);
+		System.out.println(shapeService.getCircle().getName());
+	}
+}
---
+++ src/org/roba/springaop/service/ShapeService.java
@@ -0,0 +1,26 @@
+package org.roba.springaop.service;
+
+import org.roba.springaop.model.Circle;
+import org.roba.springaop.model.Triangle;
+
+public class ShapeService {
+	private Triangle triangle;
+	private Circle circle;
+
+	public Triangle getTriangle() {
+		return triangle;
+	}
+
+	public void setTriangle(Triangle triangle) {
+		this.triangle = triangle;
+	}
+
+	public Circle getCircle() {
+		return circle;
+	}
+
+	public void setCircle(Circle circle) {
+		this.circle = circle;
+	}
+
+}
