a1ed352edfe61891ed1059f9310e4ac387c68906
achujel1 <achujel1@gmail.com>
2015-07-03 19:05:55 +0300

    Revert "Revert "Part 31. After Advice Types""
    This reverts commit 691a98949e0ce48e46064f397ef5af2f4ebb048e.

--- src/org/roba/springaop/model/Circle.java
+++ src/org/roba/springaop/model/Circle.java
@@ -9,6 +9,18 @@ public class Circle {
 
 	public void setName(String name) {
 		this.name = name;
+		// Adding a print line message to see which values have been initialized
+		// by Spring
+		System.out.println("Circle setter called. Argument: " + name);
+		throw (new RuntimeException());
+	}
+
+	public String setNameandReturn(String name) {
+		this.name = name;
+		// Adding a print line message to see which values have been initialized
+		// by Spring
+		System.out.println("Circle setter called. Argument: " + name);
+		return name;
 	}
 
 }
